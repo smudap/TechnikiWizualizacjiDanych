b1c7fa43028f3a489c4b44923f0647911482197d
MazurMaksymilian <mazurmaksymilian@gmail.com>
2016-10-10 01:43:12 +0200

    Delete MMazur-PD1.R

--- MINI_2016/PraceDomowe/Tydzien01/MMazur-PD1.R
+++
@@ -1,9 +0,0 @@
-
-
-Statystyki <- read.csv2("C:/Users/Max/Desktop/HendoBis.csv", sep=';', header=TRUE)
-
-ggplot(Statystyki,aes(x = Round)) + 
-  geom_bar(aes(weight = StrikesThrown, fill = Fighter), position = "dodge") +
-  geom_bar(aes(weight = SignStrikesLanded, fill = Fighter), position = "dodge") + 
-  scale_fill_manual(values = c(alpha("#1F78B4", 0.6), alpha("#FF0000", 0.6))) +
-  ylab("Strikes Thrown/Landed")
